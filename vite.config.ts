bbf63381850a556fcb45804dd52e42c984499c14
invers9 <invers1on.crc@gmail.com>
2025-08-22 02:16:22 +0300

    fixed name of out package

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr'
 export default defineConfig({
   assetsInclude: ['**/*.woff2'],
   build: {
-    outDir: 'milanuncious'
+    outDir: 'milanuncios'
   },
   plugins: [react(), svgr()]
 })
